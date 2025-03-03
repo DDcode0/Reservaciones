8e3ef436e2b600a02620a5f818fea9c90daf840b
K <60045948+DDcode0@users.noreply.github.com>
2025-03-02 20:02:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,6 @@ def editar_reserva(id):
     guardar_reservas(reservas)
     return redirect(url_for("home"))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
